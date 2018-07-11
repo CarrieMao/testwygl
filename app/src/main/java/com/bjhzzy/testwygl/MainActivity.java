b4a3bbe0085f6cd30b62e6b8dcd0346444c13c7d
e <yugang@hotmail.com>
2018-07-11 15:52:51 +0800

    Initial commit

---
+++ app/src/main/java/com/bjhzzy/testwygl/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bjhzzy.testwygl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //创建
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    //程序运行
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+}
